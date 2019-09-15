94cd58a68d462fc46dc0ba3bfb19623a19a4d722
XpekT <xpektpt@gmail.com>
2019-09-15 12:10:57 +0100

    pagination now remembers search terms

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
     cities: [
       "Alcobaça","Alfena","Almada","Almeirim","Alverca do Ribatejo","Amadora","Amarante","Amora","Anadia","Angra do Heroísmo","Aveiro","Benavente","Barcelos","Barreiro","Beja","Borba","Braga","Bragança","Caldas da Rainha","Câmara de Lobos","Caniço","Cantanhede","Cartaxo","Castelo Branco","Chaves","Coimbra","Costa da Caparica","Covilhã","Elvas","Entroncamento","Ermesinde","Esmoriz","Espinho","Esposende","Estarreja","Estremoz","Évora","Fafe","Faro","Fátima","Felgueiras","Figueira da Foz","Fiães","Freamunde","Funchal","Fundão","Gafanha da Nazaré","Gandra","Gondomar","Gouveia","Guarda","Guimarães","Horta","Ílhavo","Lagoa","Lagos","Lamego","Leiria","Lisboa","Lixa","Loulé","Loures","Lourosa","Macedo de Cavaleiros","Machico","Maia","Mangualde","Marco de Canaveses","Marinha Grande","Matosinhos","Mealhada","Mêda","Miranda do Douro","Mirandela","Montemor-o-Novo","Montijo","Moura","Odivelas","Olhão","Oliveira de Azeméis","Oliveira do Bairro","Oliveira do Hospital","Ourém","Ovar","Paços de Ferreira","Paredes","Penafiel","Peniche","Peso da Régua","Pinhel","Pombal","Ponta Delgada","Ponte de Sor","Portalegre","Portimão","Porto","Póvoa de Santa Iria","Póvoa de Varzim","Praia da Vitória","Quarteira","Queluz","Rebordosa","Reguengos de Monsaraz","Ribeira Grande","Rio Maior","Rio Tinto","Sabugal","Sacavém","Samora Correia","Santa Comba Dão","Santa Cruz","Santa Maria da Feira","Santana","Santarém","Santiago do Cacém","Santo Tirso","São João da Madeira","São Mamede de Infesta","São Pedro do Sul","Lordelo","Seia","Seixal","Senhora da Hora","Serpa","Setúbal","Silves","Sines","Tarouca","Tavira","Tomar","Tondela","Torres Novas","Torres Vedras","Trancoso","Trofa","Valbom","Vale de Cambra","Valença","Valongo","Valpaços","Vendas Novas","Viana do Castelo","Vila Baleira","Vila do Conde","Vila Franca de Xira","Vila Nova de Famalicão","Vila Nova de Foz Côa","Vila Nova de Gaia","Vila Nova de Santo André","Vila Real","Vila Real de Santo António","Viseu"
     ],
-    city: null
+    city: null,
+    search: null
   }
 
   config = {
@@ -56,17 +57,32 @@ class App extends Component {
         loading: true,
         pageNum: prevState.pageNum + 1
       }))
-  
-      let { html } = await getPage(this.config, this.state.pageNum + 1, this.state.city)
       
-      let { error, data, loading } = preparePage(html)
-  
-      this.setState((prevState, prevProps) => ({
-        ...prevState,
-        error,
-        data,
-        loading
-      }))
+      if(this.state.search) {
+        
+        let { html } = await getPage(this.config, this.state.pageNum + 1, this.state.search)
+        let { error, data, loading } = preparePage(html)
+    
+        this.setState((prevState, prevProps) => ({
+          ...prevState,
+          error,
+          data,
+          loading
+        }))
+      
+      } else {
+
+        let { html } = await getPage(this.config, this.state.pageNum + 1, this.state.city)
+        let { error, data, loading } = preparePage(html)
+    
+        this.setState((prevState, prevProps) => ({
+          ...prevState,
+          error,
+          data,
+          loading
+        }))
+
+      }
       
     } else {
 
@@ -130,7 +146,7 @@ class App extends Component {
 
     let city = e.target.value
     
-    this.setState({ city, loading: true, pageNum: 1 })
+    this.setState({ city, loading: true, pageNum: 1, search: null })
 
     let { html } = await getPage(this.config, 1, city)
 
@@ -152,17 +168,23 @@ class App extends Component {
       this.setState({ loading: true })
 
       let query = e.target.value.trimLeft().replace(' ', '+')
+
+      if(query.trim().length > 0) {
+
+        let { html } = await getPage(this.config, 1, query)
+    
+        let { error, data, loading } = preparePage(html)
+    
+        this.setState((prevState, prevProps) => ({
+          ...prevState,
+          search: query,
+          error,
+          data,
+          loading
+        }))
+      
+      }
   
-      let { html } = await getPage(this.config, 1, query)
-  
-      let { error, data, loading } = preparePage(html)
-  
-      this.setState((prevState, prevProps) => ({
-        ...prevState,
-        error,
-        data,
-        loading
-      }))
     
     }
 
